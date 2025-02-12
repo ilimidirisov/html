868df775eaf02d5391230be344a077d367eeb175
ilim <idirisov2007ilim@gmail.com>
2025-02-12 15:08:17 +0600

    html

---
+++ script.js
@@ -0,0 +1,21 @@
+const mark = 2;
+switch (mark) {
+    case 1:
+        console.log('u have to start working harder');
+        break;
+    case 2:
+        console.log('u need to work hard');
+        break;
+    case 3:
+        console.log('make little bit effort');
+        break;
+    case 4:
+        console.log('work is good');
+        break;
+    case 5:
+        console.log('well done ');
+        break;
+    default:
+        console.log('we have no such mark');    
+
+}
